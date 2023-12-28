2ee3c31a3ce71cab71172d47a4806e0138af28fc
memoryBangwool <jihyuneee01@gmail.com>
2023-12-28 16:56:42 +0900

    [setting] add navigation dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,7 +25,7 @@ android {
             isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
+                "proguard-rules.pro",
             )
         }
     }
@@ -56,13 +56,14 @@ dependencies {
     implementation(libs.activity.compose)
     implementation(platform(libs.compose.bom))
 
-    //appcompat implementation
+    // appcompat implementation
     implementation(libs.appcompat)
     implementation(libs.constraintlayout)
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation(libs.google.material)
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -70,4 +71,17 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    // Kotlin
+    implementation("androidx.navigation:navigation-fragment-ktx:$(libs.navigation)")
+    implementation("androidx.navigation:navigation-ui-ktx:$(libs.navigation)")
+
+    // Feature module Support
+    implementation("androidx.navigation:navigation-dynamic-features-fragment:$(libs.navigation)")
+
+    // Testing Navigation
+    androidTestImplementation("androidx.navigation:navigation-testing:$(libs.navigation)")
+
+    // Jetpack Compose Integration
+    implementation("androidx.navigation:navigation-compose:$(libs.navigation)")
 }
---
+++ app/src/main/java/com/hmh/hamyeonham/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.hmh.hamyeonham
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel(){
+
+}

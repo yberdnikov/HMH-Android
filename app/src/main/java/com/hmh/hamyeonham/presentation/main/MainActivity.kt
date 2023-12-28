4a79dc23b6e693173170a002bb0591d1b9515fa7
memoryBangwool <jihyuneee01@gmail.com>
2023-12-28 17:55:44 +0900

    [feat] navigation 추가

---
+++ app/src/main/java/com/hmh/hamyeonham/presentation/main/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.hmh.hamyeonham.presentation.main
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.hmh.hamyeonham.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

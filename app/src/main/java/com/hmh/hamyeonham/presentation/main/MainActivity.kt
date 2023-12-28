d4f614d24235b61499fd8cef49c963694a1266be
memoryBangwool <jihyuneee01@gmail.com>
2023-12-28 19:53:35 +0900

    [feat] navigation 추가

--- app/src/main/java/com/hmh/hamyeonham/presentation/main/MainActivity.kt
+++ app/src/main/java/com/hmh/hamyeonham/presentation/main/MainActivity.kt
@@ -2,12 +2,17 @@ package com.hmh.hamyeonham.presentation.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.hmh.hamyeonham.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        val navHostFragment =
+//            supportFragmentManager.findFragmentById(R.id.nhf_main) as NavHostFragment
+//        val navController = navHostFragment.navController
+//        val bottomNav = findViewById<BottomNavigationView>(R.id.btn_main)
+//        bottomNav.setupWithNavController(navController)
     }
 }

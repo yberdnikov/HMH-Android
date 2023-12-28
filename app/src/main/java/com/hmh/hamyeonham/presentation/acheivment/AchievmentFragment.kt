9f17bdc1e03cd84db05524d44d240506eefadc7f
memoryBangwool <jihyuneee01@gmail.com>
2023-12-28 19:53:35 +0900

    [feat] navigation 추가

--- app/src/main/java/com/hmh/hamyeonham/presentation/acheivment/AchievmentFragment.kt
+++ app/src/main/java/com/hmh/hamyeonham/presentation/acheivment/AchievmentFragment.kt
@@ -1,4 +1,4 @@
-package com.hmh.hamyeonham.presentation.mypage
+package com.hmh.hamyeonham.presentation.acheivment
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,10 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.hmh.hamyeonham.R
 
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-class UsagestatisticsFragment : Fragment() {
+class AchievmentFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -29,7 +26,7 @@ class UsagestatisticsFragment : Fragment() {
         savedInstanceState: Bundle?,
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_usagestatistics, container, false)
+        return inflater.inflate(R.layout.fragment_achievment, container, false)
     }
 
 //    companion object {

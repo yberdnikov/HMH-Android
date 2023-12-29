eb98b6adacee96d3996b0ac89c7c6e4b1b485c60
kangyuri1114 <yuri3334@gmail.com>
2023-12-29 21:47:01 +0900

    [feat/splash] fade out 애니메이션 적용

--- app/src/main/java/com/hmh/hamyeonham/MainActivity.kt
+++ app/src/main/java/com/hmh/hamyeonham/MainActivity.kt
@@ -2,10 +2,34 @@ package com.hmh.hamyeonham
 
 import android.app.Activity
 import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import androidx.core.splashscreen.SplashScreen
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val splashScreen = installSplashScreen()
+        initSplashAnimation(splashScreen)
+    }
+
+    private fun initSplashAnimation(splashScreen: SplashScreen) {
+        splashScreen.setOnExitAnimationListener { splashScreenViewProvider ->
+            val splashScreenView = splashScreenViewProvider.view
+            val fadeOut = AnimationUtils.loadAnimation(this, R.anim.fade_out)
+
+            fadeOut.setAnimationListener(object : Animation.AnimationListener {
+                override fun onAnimationStart(animation: Animation) {}
+                override fun onAnimationEnd(animation: Animation) {
+                    splashScreenViewProvider.remove()
+                }
+
+                override fun onAnimationRepeat(animation: Animation) {}
+            })
+            splashScreenView.startAnimation(fadeOut)
+        }
     }
 }

14acbfb44d654bb973128a8cd9da346434f0e637
KwakEuiJin <yuijin0909@gmail.com>
2023-12-30 16:54:08 +0900

    [setting]: Fix ActivityExt

--- core/common/src/main/java/com/hmh/hamyeonham/common/activity/ActivityExt.kt
+++ core/common/src/main/java/com/hmh/hamyeonham/common/activity/ActivityExt.kt
@@ -1,32 +1,11 @@
 package com.hmh.hamyeonham.common.activity
 
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.commit
 
-fun AppCompatActivity.showLoading() {
-    supportFragmentManager.commit(allowStateLoss = true) {
-        add(LoadingProgressIndicator.newInstance(), LoadingProgressIndicator.TAG)
-    }
-}
-
-fun AppCompatActivity.hideLoading() {
-    supportFragmentManager.findFragmentByTag(LoadingProgressIndicator.TAG)?.let { fragment ->
-        supportFragmentManager.commit(allowStateLoss = true) {
-            remove(fragment)
-        }
-    }
-}
-
-fun AppCompatActivity.showError() {
-    supportFragmentManager.commit(allowStateLoss = true) {
-        add(ErrorFullScreenDialogFragment.newInstance(), ErrorFullScreenDialogFragment.TAG)
-    }
-}
-
-fun AppCompatActivity.hideError() {
-    supportFragmentManager.findFragmentByTag(ErrorFullScreenDialogFragment.TAG)?.let { fragment ->
-        supportFragmentManager.commit(allowStateLoss = true) {
-            remove(fragment)
-        }
+fun AppCompatActivity.replaceFragment(containerViewId: Int, fragment: Fragment) {
+    supportFragmentManager.commit {
+        replace(containerViewId, fragment)
     }
 }

fb58b64bd6adfff3840331e2aadf3f4c5641a034
kangyuri1114 <yuri3334@gmail.com>
2024-01-02 01:54:57 +0900

    [feat]: LoginActivity 생성

--- feature/login/build.gradle.kts
+++ feature/login/build.gradle.kts
@@ -1,6 +1,7 @@
 @Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
 plugins {
     hmh("feature")
+    alias(libs.plugins.kotlin.android)
 }
 
 android {
@@ -9,4 +10,7 @@ android {
 
 dependencies {
     implementation(projects.core.common)
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.constraintlayout)
 }

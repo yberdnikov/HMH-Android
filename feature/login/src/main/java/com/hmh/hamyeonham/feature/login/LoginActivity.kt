394008b414af35912111842b57a8db9ddaa59698
kangyuri1114 <yuri3334@gmail.com>
2024-01-02 01:57:59 +0900

    [feat]: LoginActivity viewBinding

--- feature/login/src/main/java/com/hmh/hamyeonham/feature/login/LoginActivity.kt
+++ feature/login/src/main/java/com/hmh/hamyeonham/feature/login/LoginActivity.kt
@@ -2,10 +2,13 @@ package com.hmh.hamyeonham.feature.login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.hmh.hamyeonham.feature.login.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityLoginBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(binding.root)
     }
 }

db86698647765de291358f9641b65005f3c995b2
KwakEuiJin <yuijin0909@gmail.com>
2024-01-01 23:34:58 +0900

    [fix]: Request Permission

--- feature/onboarding/src/main/java/com/hmh/hamyeonham/feature/onboarding/OnBoardingAccessibilityService.kt
+++ feature/onboarding/src/main/java/com/hmh/hamyeonham/feature/onboarding/OnBoardingAccessibilityService.kt
@@ -1,15 +1,19 @@
 package com.hmh.hamyeonham.feature.onboarding
 
 import android.accessibilityservice.AccessibilityService
+import android.util.Log
+import android.view.accessibility.AccessibilityEvent
 import com.hmh.hamyeonham.common.context.toast
 
 class OnBoardingAccessibilityService : AccessibilityService() {
-    override fun onInterrupt() {
-        toast("서비스 onInterrupt ")
+    override fun onAccessibilityEvent(event: AccessibilityEvent) {
+        if (event.eventType == AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED) {
+            val packageName = event.packageName ?: return
+            Log.d("AccessibilityService", "현재 실행 중인 앱 패키지: $packageName")
+        }
     }
 
-    override fun onAccessibilityEvent(event: android.view.accessibility.AccessibilityEvent?) {
-        toast("서비스 onAccessibilityEvent ")
+    override fun onInterrupt() {
 
     }
 }
--- feature/onboarding/src/main/java/com/hmh/hamyeonham/feature/onboarding/OnBoardingActivity.kt
+++ feature/onboarding/src/main/java/com/hmh/hamyeonham/feature/onboarding/OnBoardingActivity.kt
@@ -1,104 +1,44 @@
 package com.hmh.hamyeonham.feature.onboarding
 
 import android.accessibilityservice.AccessibilityService
-import android.app.AppOpsManager
+import android.app.ActivityManager
+import android.content.ComponentName
+import android.content.Context
 import android.content.Intent
-import android.content.pm.PackageInfo
-import android.content.pm.PackageManager
-import android.os.Build
 import android.os.Bundle
 import android.provider.Settings
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import com.hmh.hamyeonham.common.context.toast
 import com.hmh.hamyeonham.feature.onboarding.databinding.ActivityOnBoardingBinding
 
 class OnBoardingActivity : AppCompatActivity() {
-
-    override fun onResume() {
-        super.onResume()
-        if (hasUsageStatsPermission()) {
-            toast("권한이 허용되어 있음")
-            requirePermission()
-        } else {
-            toast("권한이 허용되어 있지 않음")
-            requirePermission()
-        }
-    }
 
     private lateinit var binding: ActivityOnBoardingBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityOnBoardingBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-        // 설치된 앱 목록 가져오기
-        val installedApps: List<PackageInfo> =
-            packageManager.getInstalledPackages(PackageManager.GET_META_DATA)
-        Log.d("TAG", "installedApps: $installedApps")
-        for (packageInfo in installedApps) {
-            val appName: String = packageInfo.applicationInfo.loadLabel(packageManager).toString()
-            val packageName: String = packageInfo.packageName
-            val flags = packageInfo.applicationInfo.flags
-
-            Log.d("AppInfo", "Name: $appName, Package: $packageName, Flags: $flags")
-        }
+        requirePermission()
     }
 
     private fun requirePermission() {
         binding.btnPermission.setOnClickListener {
-            moveAppToTopInAccessibilitySettings(AccessibilityService::class.java)
-            // startActivity(Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS))
+            openAccessibilitySettingsIfNeeded()
         }
     }
 
-    private fun hasUsageStatsPermission(): Boolean {
-        val appOps = getSystemService(APP_OPS_SERVICE) as AppOpsManager
-        val mode = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-            appOps.unsafeCheckOpNoThrow(
-                AppOpsManager.OPSTR_GET_USAGE_STATS,
-                android.os.Process.myUid(),
-                packageName,
-            )
-        } else {
-            appOps.checkOpNoThrow(
-                AppOpsManager.OPSTR_GET_USAGE_STATS,
-                android.os.Process.myUid(),
-                packageName,
-            )
-        }
-        return mode == AppOpsManager.MODE_ALLOWED
-    }
-
-    // 사용 접근 권한 앱 목록 순서 변경 함수
-    private fun moveAppToTopInAccessibilitySettings(service: Class<out AccessibilityService>) {
-        val expectedId = packageName + "/" + service.canonicalName
-        Log.d("TAG1", "expectedId: $expectedId")
-        startActivity(Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS))
-
+    private fun isAccessibilityServiceEnabled(): Boolean {
+        val service = packageName + "/" + OnBoardingAccessibilityService::class.java.canonicalName
         val enabledServicesSetting = Settings.Secure.getString(
             contentResolver,
-            Settings.Secure.ENABLED_ACCESSIBILITY_SERVICES,
+            Settings.Secure.ENABLED_ACCESSIBILITY_SERVICES
         )
-        Log.d("TAG2", "enabledServicesSetting: $enabledServicesSetting")
+        return enabledServicesSetting?.contains(service) == true
+    }
 
-        if (enabledServicesSetting == null) {
-            Log.e("TAG3", "Enabled services setting is null")
-            return
+    private fun openAccessibilitySettingsIfNeeded() {
+        if (!isAccessibilityServiceEnabled()) {
+            val intent = Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS)
+            startActivity(intent)
         }
-
-        // 서비스 목록 순서 재배열
-        val serviceList = enabledServicesSetting.split(':').toMutableList()
-        serviceList.remove(expectedId)
-        serviceList.add(0, expectedId)
-        Log.d("TAG4", "serviceList: $serviceList")
-        val updatedServicesSetting = serviceList.joinToString(":")
-        Log.d("TAG5", "updatedServicesSetting: $updatedServicesSetting")
-
-        // 변경된 서비스 목록을 설정에 저장
-        Settings.Secure.putString(
-            contentResolver,
-            Settings.Secure.ENABLED_ACCESSIBILITY_SERVICES,
-            updatedServicesSetting,
-        )
     }
 }

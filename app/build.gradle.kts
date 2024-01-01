5f46d5631d08db4ea9909a094946234f568e4033
KwakEuiJin <yuijin0909@gmail.com>
2024-01-01 23:57:45 +0900

    [fix]: Debug KeyStore

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,13 @@ android {
     }
 
     buildTypes {
+        debug {
+            isDebuggable = true
+            signingConfig = signingConfigs.getByName("debug")
+        }
         release {
             isMinifyEnabled = false
+            isDebuggable = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro",

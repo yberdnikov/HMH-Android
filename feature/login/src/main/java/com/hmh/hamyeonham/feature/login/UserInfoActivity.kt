d9a660d404fa9af685b92dae1fc082117c511fe2
kangyuri1114 <yuri3334@gmail.com>
2024-01-02 18:16:00 +0900

    [feat]: 로그인 성공 시 유저 정보 반환 액티비티 생성 및 세팅

--- feature/login/src/main/java/com/hmh/hamyeonham/feature/login/UserInfoActivity.kt
+++ feature/login/src/main/java/com/hmh/hamyeonham/feature/login/UserInfoActivity.kt
@@ -2,10 +2,15 @@ package com.hmh.hamyeonham.feature.login
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.hmh.hamyeonham.feature.login.databinding.ActivityUserInfoBinding
 
 class UserInfoActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityUserInfoBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityUserInfoBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_user_info)
+        setContentView(binding.root)
+
+
     }
 }
---
+++ feature/login/src/main/java/com/hmh/hamyeonham/feature/login/UserInfoViewModel.kt
@@ -0,0 +1,5 @@
+package com.hmh.hamyeonham.feature.login
+
+import androidx.lifecycle.ViewModel
+
+class UserInfoViewModel : ViewModel()

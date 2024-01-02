8e04892cbf4d994a2bed14ee2335d492a70fb595
kangyuri1114 <yuri3334@gmail.com>
2024-01-02 19:24:28 +0900

    [refact]: 카카오 로그인 코드 리팩토링

--- feature/login/src/main/java/com/hmh/hamyeonham/feature/login/LoginActivity.kt
+++ feature/login/src/main/java/com/hmh/hamyeonham/feature/login/LoginActivity.kt
@@ -1,10 +1,9 @@
 package com.hmh.hamyeonham.feature.login
 
-import android.content.ContentValues.TAG
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.hmh.hamyeonham.common.context.toast
 import com.hmh.hamyeonham.feature.login.databinding.ActivityLoginBinding
 import com.kakao.sdk.auth.model.OAuthToken
 import com.kakao.sdk.common.model.ClientError
@@ -14,51 +13,49 @@ import com.kakao.sdk.user.UserApiClient
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
 
+    private val callback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
+        when {
+            error != null -> {
+            }
+
+            token != null -> {
+                moveToUserInfoActivity()
+            }
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityLoginBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        loginWithKakaoTalk()
+        binding.btnLogin.setOnClickListener {
+            loginWithKakaoApp()
+        }
     }
 
-    // 로그인 조합 예제
-    private fun loginWithKakaoTalk() {
-        binding.btnLogin.setOnClickListener {
-            // 카카오계정으로 로그인 공통 callback 구성
-// 카카오톡으로 로그인 할 수 없어 카카오계정으로 로그인할 경우 사용됨
-            val callback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
+    private fun loginWithKakaoApp() {
+        if (UserApiClient.instance.isKakaoTalkLoginAvailable(this)) {
+            UserApiClient.instance.loginWithKakaoTalk(this) { token, error ->
                 if (error != null) {
-                    Log.e(TAG, "카카오계정으로 로그인 실패", error)
+                    toast("카카오 로그인 실패")
+                    if (error is ClientError && error.reason == ClientErrorCause.Cancelled) {
+                        toast("다시 로그인 해주세요.")
+                        return@loginWithKakaoTalk
+                    }
+                    loginWithKakaoAccount()
                 } else if (token != null) {
-                    Log.i(TAG, "카카오계정으로 로그인 성공 ${token.accessToken}")
+                    toast("카카오 로그인 성공")
                     moveToUserInfoActivity()
                 }
             }
-
-// 카카오톡이 설치되어 있으면 카카오톡으로 로그인, 아니면 카카오계정으로 로그인
-            if (UserApiClient.instance.isKakaoTalkLoginAvailable(this)) {
-                UserApiClient.instance.loginWithKakaoTalk(this) { token, error ->
-                    if (error != null) {
-                        Log.e(TAG, "카카오톡으로 로그인 실패", error)
-
-                        // 사용자가 카카오톡 설치 후 디바이스 권한 요청 화면에서 로그인을 취소한 경우,
-                        // 의도적인 로그인 취소로 보고 카카오계정으로 로그인 시도 없이 로그인 취소로 처리 (예: 뒤로 가기)
-                        if (error is ClientError && error.reason == ClientErrorCause.Cancelled) {
-                            return@loginWithKakaoTalk
-                        }
-
-                        // 카카오톡에 연결된 카카오계정이 없는 경우, 카카오계정으로 로그인 시도
-                        UserApiClient.instance.loginWithKakaoAccount(this, callback = callback)
-                    } else if (token != null) {
-                        Log.i(TAG, "카카오톡으로 로그인 성공 ${token.accessToken}")
-                        moveToUserInfoActivity()
-                    }
-                }
-            } else {
-                UserApiClient.instance.loginWithKakaoAccount(this, callback = callback)
-            }
+        } else {
+            loginWithKakaoAccount()
         }
+    }
+
+    private fun loginWithKakaoAccount() {
+        UserApiClient.instance.loginWithKakaoAccount(this, callback = callback)
     }
 
     private fun moveToUserInfoActivity() {

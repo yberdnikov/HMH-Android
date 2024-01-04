f3e4b47c9d52bd2d6159c62166765d2c3a944a37
KwakEuiJin <yuijin0909@gmail.com>
2024-01-04 21:53:38 +0900

    [fix]: Fix Merge Error

--- app/src/main/java/com/hmh/hamyeonham/SampleActivity.kt
+++ app/src/main/java/com/hmh/hamyeonham/SampleActivity.kt
@@ -1,27 +1,21 @@
 package com.hmh.hamyeonham
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.hmh.hamyeonham.common.view.viewBinding
 import com.hmh.hamyeonham.databinding.ActivitySampleBinding
-import com.hmh.hamyeonham.statistics.StaticsActivity
 import dagger.hilt.android.AndroidEntryPoint
-import com.hmh.hamyeonham.feature.login.LoginActivity
-import com.kakao.sdk.common.KakaoSdk
-import com.hmh.hamyeonham.feature.main.MainActivity
 
 @AndroidEntryPoint
 class SampleActivity : AppCompatActivity() {
-    private lateinit var binding: ActivitySampleBinding
+    private val binding by viewBinding(ActivitySampleBinding::inflate)
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        binding = ActivitySampleBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-
         val splashScreen = installSplashScreen()
         initSplashAnimation(splashScreen)
         setContentView(binding.root)

c56bb96e70ed619ee2ebe968bb29ab10b38a0e60
kangyuri1114 <yuri3334@gmail.com>
2024-01-04 17:24:38 +0900

    [feat]: 다음 버튼 클릭 시 프래그먼트2로 이동

--- feature/onboarding/src/main/java/com/hmh/hamyeonham/feature/onboarding/OnBoardingActivity.kt
+++ feature/onboarding/src/main/java/com/hmh/hamyeonham/feature/onboarding/OnBoardingActivity.kt
@@ -14,10 +14,43 @@ class OnBoardingActivity : AppCompatActivity() {
         binding = ActivityOnBoardingBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        addFragment(OnboardingSelectDataFragment(), "fragment1_tag")
+        val (fragment1, fragment2) = setOnboardingFragment()
+
+        addFragment(fragment1, "fragment1_tag")
+        binding.btnOnboardingNext.setOnClickListener {
+            replaceFragment(fragment2, "fragment2_tag")
+        }
+    }
+
+    private fun setOnboardingFragment(): Pair<OnboardingSelectDataFragment, OnboardingSelectDataFragment> {
+        val fragment1 = OnboardingSelectDataFragment.newInstance(
+            "하루 평균 휴대폰을 \n" +
+                "얼마나 사용하시나요?",
+            "1-4시간",
+            "4-8시간",
+            "8-12시간",
+            "12시간 이상",
+        )
+
+        val fragment2 = OnboardingSelectDataFragment.newInstance(
+            "휴대폰을 사용할 때\n" +
+                "어떤 문제를 겪고 계시나요?",
+            "중독이 너무 심해요",
+            "무의식적으로 사용하게 돼요",
+            "스스로 제어가 안돼요",
+            "일상생활에 영향을 끼쳐요",
+        )
+        return Pair(fragment1, fragment2)
     }
 
     private fun addFragment(fragment: Fragment, tag: String) {
+        supportFragmentManager.beginTransaction()
+            .replace(binding.flOnboardingContainer.id, fragment, tag)
+            .addToBackStack(null)
+            .commit()
+    }
+
+    private fun replaceFragment(fragment: Fragment, tag: String) {
         supportFragmentManager.beginTransaction()
             .replace(binding.flOnboardingContainer.id, fragment, tag)
             .addToBackStack(null)
--- feature/onboarding/src/main/java/com/hmh/hamyeonham/feature/onboarding/OnboardingSelectDataFragment.kt
+++ feature/onboarding/src/main/java/com/hmh/hamyeonham/feature/onboarding/OnboardingSelectDataFragment.kt
@@ -11,6 +11,26 @@ class OnboardingSelectDataFragment : Fragment() {
     private var _binding: FragmentOnboardingSelectDataBinding? = null
     private val binding get() = _binding!!
 
+    companion object {
+        private const val ARG_QUESTION = "question"
+        private const val ARG_BTN1 = "btn1"
+        private const val ARG_BTN2 = "btn2"
+        private const val ARG_BTN3 = "btn3"
+        private const val ARG_BTN4 = "btn4"
+
+        fun newInstance(question: String, btn1: String, btn2: String, btn3: String, btn4: String): OnboardingSelectDataFragment {
+            val fragment = OnboardingSelectDataFragment()
+            val args = Bundle()
+            args.putString(ARG_QUESTION, question)
+            args.putString(ARG_BTN1, btn1)
+            args.putString(ARG_BTN2, btn2)
+            args.putString(ARG_BTN3, btn3)
+            args.putString(ARG_BTN4, btn4)
+            fragment.arguments = args
+            return fragment
+        }
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -23,11 +43,17 @@ class OnboardingSelectDataFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.tvOnbaordingSelectDataQuestion.text = "하루 평균 휴대폰을 \n얼마나 사용하시나요?"
-        binding.btnOnboardingSelectData1.text = "1-4 시간"
-        binding.btnOnboardingSelectData2.text = "5-6 시간"
-        binding.btnOnboardingSelectData3.text = "7-8 시간"
-        binding.btnOnboardingSelectData4.text = "9-10 시간"
+        val question = requireArguments().getString(ARG_QUESTION)
+        val btn1 = requireArguments().getString(ARG_BTN1)
+        val btn2 = requireArguments().getString(ARG_BTN2)
+        val btn3 = requireArguments().getString(ARG_BTN3)
+        val btn4 = requireArguments().getString(ARG_BTN4)
+
+        binding.tvOnbaordingSelectDataQuestion.text = question
+        binding.btnOnboardingSelectData1.text = btn1
+        binding.btnOnboardingSelectData2.text = btn2
+        binding.btnOnboardingSelectData3.text = btn3
+        binding.btnOnboardingSelectData4.text = btn4
     }
 
     override fun onDestroyView() {

e9cd4c4e0febac7026e756a1191316b5fa662412
kangyuri1114 <yuri3334@gmail.com>
2024-01-07 01:20:59 +0900

    [fix]: 회원가입 완료 화면 프래그먼트에서 액티비티로 교체

---
+++ feature/onboarding/src/main/java/com/hmh/hamyeonham/feature/onboarding/DoneSingUpActivity.kt
@@ -0,0 +1,11 @@
+package com.hmh.hamyeonham.feature.onboarding
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class DoneSingUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_done_sing_up)
+    }
+}

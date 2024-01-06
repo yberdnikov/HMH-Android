f6eb69ecc06afc09d28b1cd820808cbfe3096a5d
memoryBangwool <jihyuneee01@gmail.com>
2024-01-06 14:24:03 +0900

    [feat] : design system 설계

---
+++ core/design_system/build.gradle.kts
@@ -0,0 +1,11 @@
+@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
+plugins {
+    hmh("feature")
+}
+
+android {
+    namespace = "com.hmh.hamyeonham.core.design_system"
+}
+
+dependencies {
+}

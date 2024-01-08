dbb989168c729007cab7a96c0487814f72f9c111
memoryBangwool <jihyuneee01@gmail.com>
2024-01-08 22:52:20 +0900

    [feat] : 이용시간 통계 뷰 디자인 테마 적용

--- feature/statistics/build.gradle.kts
+++ feature/statistics/build.gradle.kts
@@ -15,7 +15,7 @@ dependencies {
 
     // Core
     implementation(projects.core.common)
-    implementation(projects.core.designSystem)
+    implementation(projects.core.designsystem)
 
     // others
     implementation(libs.appcompat)

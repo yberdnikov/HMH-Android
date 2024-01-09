a477ffc2e7c5101226fe4a12f91eb7c104a2f0f8
kangyuri1114 <yuri3334@gmail.com>
2024-01-10 00:08:46 +0900

    [feat]: set 넘버 피커 확장 함수 생성

--- feature/onboarding/src/main/java/com/hmh/hamyeonham/feature/onboarding/fragment/OnBoardingSelectScreenTimeFragment.kt
+++ feature/onboarding/src/main/java/com/hmh/hamyeonham/feature/onboarding/fragment/OnBoardingSelectScreenTimeFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.NumberPicker
 import androidx.fragment.app.Fragment
 import com.hmh.hamyeonham.common.view.setupScreentimeGoalRange
 import com.hmh.hamyeonham.common.view.viewBinding
@@ -21,7 +20,7 @@ class OnBoardingSelectScreenTimeFragment : Fragment() {
         return FragmentOnBoardingSelectScreentimeBinding.inflate(
             inflater,
             container,
-            false
+            false,
         ).root
     }
 
@@ -29,6 +28,5 @@ class OnBoardingSelectScreenTimeFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         binding.npOnboardingScreentimeGoal.setupScreentimeGoalRange(1, 6)
-
     }
 }

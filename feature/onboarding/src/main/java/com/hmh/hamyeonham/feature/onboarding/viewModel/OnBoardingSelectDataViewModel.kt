52bc1fea2d23cc17a3e9c68ee7942bbfe48f6710
kangyuri1114 <yuri3334@gmail.com>
2024-01-10 03:57:01 +0900

    [fix]: 변경된 라이팅으로 수정

--- feature/onboarding/src/main/java/com/hmh/hamyeonham/feature/onboarding/viewModel/OnBoardingSelectDataViewModel.kt
+++ feature/onboarding/src/main/java/com/hmh/hamyeonham/feature/onboarding/viewModel/OnBoardingSelectDataViewModel.kt
@@ -19,20 +19,21 @@ class OnBoardingSelectDataViewModel @Inject constructor() : ViewModel() {
 
     fun initQuestionData(fragmentType: OnBoardingFragmentType) {
         val onBoardingQuestionTime = OnBoardingQuestion(
-            "하루 평균 휴대폰을\n얼마나 사용하시나요?",
+
+            "하루 평균 휴대폰을\n얼마나 사용하나요?",
             "",
             listOf("1-4시간", "4-8시간", "8-12시간", "12시간 이상"),
         )
 
         val onBoardingQuestionProblem = OnBoardingQuestion(
-            "휴대폰을 사용할 때\n어떤 문제를 겪고 계시나요?",
-            "해당 문항은 최대 2개의 복수 응답이 가능해요",
-            listOf("중독이 너무 심해요", "무의식적으로 사용하게 돼요", "스스로 제어가 안돼요", "일상생활에 영향을 끼쳐요"),
+            "휴대폰을 사용할 때\n어떤 문제를 겪고 있나요?",
+            "해당 문항은 최대 2개까지 선택할 수 있어요",
+            listOf("일상생활에 영향을 끼쳐요", "이용 시간이 스스로 제어되지 않아요", "특정 앱에 수시로 접속하게 돼요", "중독을 탈출하려고 노력해도 잘 안 돼요"),
         )
 
         val onBoardingChallengePeriod = OnBoardingQuestion(
             "챌린지 기간을 선택해주세요",
-            "첫 챌린지로 가볍게 도전하기 좋은 7일을 추천해요!",
+            "첫 챌린지로 가볍게 도전하기 좋은 7일을 추천해요",
             listOf("7일", "14일", "21일", "30일"),
         )
 

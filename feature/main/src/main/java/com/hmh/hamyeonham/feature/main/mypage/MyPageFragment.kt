aac64142f6c4d9bfdf0f3601a5243e5f658d9c20
memoryBangwool <jihyuneee01@gmail.com>
2024-01-10 00:02:45 +0900

    [feat] : fragment 내에서 userInfo 구독기능 추가

--- feature/main/src/main/java/com/hmh/hamyeonham/feature/main/mypage/MyPageFragment.kt
+++ feature/main/src/main/java/com/hmh/hamyeonham/feature/main/mypage/MyPageFragment.kt
@@ -6,12 +6,14 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.lifecycleScope
 import com.hmh.hamyeonham.common.view.viewBinding
 import com.hmh.hamyeonham.feature.main.MainViewModel
 import com.hmh.hamyeonham.feature.main.databinding.FragmentMyPageBinding
+import com.hmh.hamyeonham.userinfo.model.UserInfo
+import kotlinx.coroutines.launch
 
 class MyPageFragment : Fragment() {
-
     private val binding by viewBinding(FragmentMyPageBinding::bind)
     private val mainViewModel by activityViewModels<MainViewModel>()
     override fun onCreateView(
@@ -20,5 +22,23 @@ class MyPageFragment : Fragment() {
         savedInstanceState: Bundle?,
     ): View {
         return FragmentMyPageBinding.inflate(inflater, container, false).root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        collectAndBindUserInfo()
+    }
+
+    private fun collectAndBindUserInfo() {
+        lifecycleScope.launch {
+            mainViewModel.userInfo.collect {
+                bindViewWithUserInfo(it)
+            }
+        }
+    }
+
+    private fun bindViewWithUserInfo(userInfo: UserInfo) {
+        binding.tvMypageName.text = userInfo.name
+        binding.tvMypagePoint.text = userInfo.point.toString()
     }
 }

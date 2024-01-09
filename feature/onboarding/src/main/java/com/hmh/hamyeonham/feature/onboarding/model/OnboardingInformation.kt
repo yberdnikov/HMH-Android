d9e094a5e2a9e41c18a6e7ad45391bb8594a5781
kangyuri1114 <yuri3334@gmail.com>
2024-01-09 17:03:57 +0900

    [fix]: OnboardingInformation 저장할 데이터 형식 수정

---
+++ feature/onboarding/src/main/java/com/hmh/hamyeonham/feature/onboarding/model/OnboardingInformation.kt
@@ -0,0 +1,23 @@
+package com.hmh.hamyeonham.feature.onboarding.model
+
+data class OnboardingInformation(
+    val usuallyUseTime: String = "",
+    val problems: List<Problem> = emptyList(),
+    val challenge: Challenge = Challenge(0, 0),
+    val apps: List<App> = emptyList(),
+)
+
+data class Problem(
+    val description1: String = "",
+    val description2: String = "",
+)
+
+data class Challenge(
+    val period: Int = -1,
+    val goalTime: Int = -1,
+)
+
+data class App(
+    val appCode: String = "",
+    val goalTime: Int = -1,
+)

624ef53c259fe6f4f06ba4ec808bfcd98c31c286
kangyuri1114 <yuri3334@gmail.com>
2024-01-09 21:40:33 +0900

    [feat]: OnboardingInformation 구조 수정

--- feature/onboarding/src/main/java/com/hmh/hamyeonham/feature/onboarding/model/OnboardingInformation.kt
+++ feature/onboarding/src/main/java/com/hmh/hamyeonham/feature/onboarding/model/OnboardingInformation.kt
@@ -5,19 +5,19 @@ data class OnboardingInformation(
     val problems: List<Problem> = emptyList(),
     val challenge: Challenge = Challenge(0, 0),
     val apps: List<App> = emptyList(),
-)
+) {
+    data class Problem(
+        val description1: String = "",
+        val description2: String = "",
+    )
 
-data class Problem(
-    val description1: String = "",
-    val description2: String = "",
-)
+    data class Challenge(
+        val period: Int = -1,
+        val goalTime: Int = -1,
+    )
 
-data class Challenge(
-    val period: Int = -1,
-    val goalTime: Int = -1,
-)
-
-data class App(
-    val appCode: String = "",
-    val goalTime: Int = -1,
-)
+    data class App(
+        val appCode: String = "",
+        val goalTime: Int = -1,
+    )
+}

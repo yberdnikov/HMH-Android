d245302eb320bda89d424e3fba2942834d092ff2
kangyuri1114 <yuri3334@gmail.com>
2024-01-10 00:07:56 +0900

    [feat]: set 넘버 피커 확장 함수 생성

--- feature/onboarding/src/main/java/com/hmh/hamyeonham/feature/onboarding/fragment/OnBoardingSelectScreenTimeFragment.kt
+++ feature/onboarding/src/main/java/com/hmh/hamyeonham/feature/onboarding/fragment/OnBoardingSelectScreenTimeFragment.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.NumberPicker
 import androidx.fragment.app.Fragment
+import com.hmh.hamyeonham.common.view.setupScreentimeGoalRange
 import com.hmh.hamyeonham.common.view.viewBinding
 import com.hmh.hamyeonham.feature.onboarding.databinding.FragmentOnBoardingSelectScreentimeBinding
 
@@ -26,9 +28,7 @@ class OnBoardingSelectScreenTimeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.run {
-            npOnboardingScreentimeGoal.minValue = 1
-            npOnboardingScreentimeGoal.maxValue = 6
-        }
+        binding.npOnboardingScreentimeGoal.setupScreentimeGoalRange(1, 6)
+
     }
 }
--- feature/onboarding/src/main/java/com/hmh/hamyeonham/feature/onboarding/fragment/OnBoardingSelectUseTimeFragment.kt
+++ feature/onboarding/src/main/java/com/hmh/hamyeonham/feature/onboarding/fragment/OnBoardingSelectUseTimeFragment.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.NumberPicker
 import androidx.fragment.app.Fragment
+import com.hmh.hamyeonham.common.view.setupScreentimeGoalRange
 import com.hmh.hamyeonham.common.view.viewBinding
 import com.hmh.hamyeonham.feature.onboarding.databinding.FragmentOnBoardingSelectUseTimeBinding
 
@@ -22,11 +24,7 @@ class OnBoardingSelectUseTimeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.run {
-            npOnboardingUseTimeGoalHour.minValue = 1
-            npOnboardingUseTimeGoalHour.maxValue = 6
-            npOnboardingUseTimeGoalMinute.minValue = 0
-            npOnboardingUseTimeGoalMinute.maxValue = 59
-        }
+        binding.npOnboardingUseTimeGoalHour.setupScreentimeGoalRange(1, 6)
+        binding.npOnboardingUseTimeGoalMinute.setupScreentimeGoalRange(0, 59)
     }
 }

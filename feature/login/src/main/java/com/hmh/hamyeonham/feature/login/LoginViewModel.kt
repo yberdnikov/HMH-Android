ae6e1b188c26fecd234abc4a0457e7307eb2b0f4
kangyuri1114 <yuri3334@gmail.com>
2024-01-11 01:15:55 +0900

    [feat]: 로그인 토큰 변수 저장 임시 구현

--- feature/login/src/main/java/com/hmh/hamyeonham/feature/login/LoginViewModel.kt
+++ feature/login/src/main/java/com/hmh/hamyeonham/feature/login/LoginViewModel.kt
@@ -1,6 +1,7 @@
 package com.hmh.hamyeonham.feature.login
 
 import android.content.Context
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import com.kakao.sdk.auth.model.OAuthToken
 import com.kakao.sdk.common.model.ClientError
@@ -11,12 +12,22 @@ import kotlinx.coroutines.flow.StateFlow
 
 class LoginViewModel : ViewModel() {
 
-    private val _isSuccessKakaoLogin = MutableStateFlow(true)
+    private val _isSuccessKakaoLogin = MutableStateFlow(false)
     val isSuccessKakaoLogin: StateFlow<Boolean> = _isSuccessKakaoLogin
+
+    private val _accessToken = MutableStateFlow<String?>(null)
+    val accessToken: StateFlow<String?> = _accessToken
+
+    private val _refreshToken = MutableStateFlow<String?>(null)
+    val refreshToken: StateFlow<String?> = _refreshToken
+
 
     private var kakaoAccountCallback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
         if (error == null && token != null) {
+            _accessToken.value = token.accessToken
+            _refreshToken.value = token.refreshToken
             _isSuccessKakaoLogin.value = true
+            Log.d("LoginViewModel", "accessToken: ${_accessToken.value}, refreshToken: ${_refreshToken.value}")
         }
     }
 
@@ -26,6 +37,8 @@ class LoginViewModel : ViewModel() {
                 _isSuccessKakaoLogin.value = false
             }
         } else if (token != null) {
+            _accessToken.value = token.accessToken
+            _refreshToken.value = token.refreshToken
             _isSuccessKakaoLogin.value = true
         }
     }

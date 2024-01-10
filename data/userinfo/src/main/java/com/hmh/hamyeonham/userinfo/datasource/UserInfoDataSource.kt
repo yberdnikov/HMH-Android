100c93fe7924634a6f043b96762bc4acf3711659
memoryBangwool <jihyuneee01@gmail.com>
2024-01-11 04:43:47 +0900

    [fix]: 힐트 오류, xml 오류 수정

--- data/userinfo/src/main/java/com/hmh/hamyeonham/userinfo/datasource/UserInfoDataSource.kt
+++ data/userinfo/src/main/java/com/hmh/hamyeonham/userinfo/datasource/UserInfoDataSource.kt
@@ -3,12 +3,10 @@ package com.hmh.hamyeonham.userinfo.datasource
 import com.hmh.hamyeonham.userinfo.model.UserInfoModel
 import javax.inject.Inject
 
-class UserInfoDataSource
-    @Inject
-    constructor() {
-        private val userInfoModel = UserInfoModel("여민서", 0)
+class UserInfoDataSource @Inject constructor() {
+    private val userInfoModel = UserInfoModel("여민서", 0)
 
-        fun getUserInfoModel(): UserInfoModel {
-            return userInfoModel
-        }
+    fun getUserInfoModel(): UserInfoModel {
+        return userInfoModel
     }
+}
--- data/userinfo/src/main/java/com/hmh/hamyeonham/userinfo/di/UserInfoModule.kt
+++ data/userinfo/src/main/java/com/hmh/hamyeonham/userinfo/di/UserInfoModule.kt
@@ -1,11 +1,9 @@
 package com.hmh.hamyeonham.userinfo.di
 
-import com.hmh.hamyeonham.userinfo.datasource.UserInfoDataSource
 import com.hmh.hamyeonham.userinfo.repository.DefaultUserInfoRepository
 import com.hmh.hamyeonham.userinfo.repository.UserInfoRepository
 import dagger.Binds
 import dagger.Module
-import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
@@ -13,20 +11,11 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 object UserInfoModule {
-
-    @Provides
-    @Singleton
-    fun provideUserInfoDataSource(): UserInfoDataSource {
-        return UserInfoDataSource()
-    }
-
     @Module
     @InstallIn(SingletonComponent::class)
     interface UserInfoBinder {
         @Binds
         @Singleton
-        fun provideUserInfoRepository(
-            userInfoRepository: DefaultUserInfoRepository,
-        ): UserInfoRepository
+        fun provideUserInfoRepository(userInfoRepository: DefaultUserInfoRepository): UserInfoRepository
     }
 }

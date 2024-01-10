621aa5f3ac63149c2f86af9ec800dae6116ea5cb
memoryBangwool <jihyuneee01@gmail.com>
2024-01-11 03:50:43 +0900

    [refact] : rename getUserInfoUseCase -> GetUserInfoUseCase

--- domain/userinfo/src/main/java/com/hmh/hamyeonham/userinfo/usecase/GetUserInfoUseCase.kt
+++ domain/userinfo/src/main/java/com/hmh/hamyeonham/userinfo/usecase/GetUserInfoUseCase.kt
@@ -4,10 +4,12 @@ import com.hmh.hamyeonham.userinfo.model.UserInfo
 import com.hmh.hamyeonham.userinfo.repository.UserInfoRepository
 import javax.inject.Inject
 
-class GetUserInfoUseCase @Inject constructor(
-    private val getUserInfo: UserInfoRepository,
-) {
-    operator fun invoke(): UserInfo {
-        return getUserInfo.getUserInfo()
+class GetUserInfoUseCase
+    @Inject
+    constructor(
+        private val getUserInfo: UserInfoRepository,
+    ) {
+        operator fun invoke(): UserInfo {
+            return getUserInfo.getUserInfo()
+        }
     }
-}

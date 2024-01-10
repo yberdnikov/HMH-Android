30d3775b871c5405ef5273bb4a7c744274c78cb5
KwakEuiJin <yuijin0909@gmail.com>
2024-01-10 20:36:09 +0900

    [feat]: Add core-network

---
+++ core/network/build.gradle.kts
@@ -0,0 +1,18 @@
+@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
+plugins {
+    hmh("feature")
+    hmh("serialization")
+}
+
+android {
+    namespace = "com.hmh.hamyeonham.core.network"
+}
+
+dependencies {
+    implementation(projects.core.common)
+    implementation(libs.security)
+    implementation(libs.bundles.retrofit)
+    implementation(platform(libs.okhttp.bom))
+    implementation(libs.okhttp.logging.interceptor)
+    implementation(libs.process.phoenix)
+}

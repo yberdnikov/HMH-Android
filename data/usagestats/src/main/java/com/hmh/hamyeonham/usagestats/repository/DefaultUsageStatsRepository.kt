0897e0ac98d143465860fdf99c948e15afc4e865
memoryBangwool <jihyuneee01@gmail.com>
2024-01-11 17:58:33 +0900

    [refact]: lint 적용

--- data/usagestats/src/main/java/com/hmh/hamyeonham/usagestats/repository/DefaultUsageStatsRepository.kt
+++ data/usagestats/src/main/java/com/hmh/hamyeonham/usagestats/repository/DefaultUsageStatsRepository.kt
@@ -5,11 +5,11 @@ import com.hmh.hamyeonham.usagestats.model.UsageStat
 import javax.inject.Inject
 
 class DefaultUsageStatsRepository @Inject constructor(
-    private val usageStatsDataSource: UsageStatsDataSource,
+    private val usageStatsDataSource: UsageStatsDataSource
 ) : UsageStatsRepository {
     override fun getUsageStats(
         startTime: Long,
-        endTime: Long,
+        endTime: Long
     ): List<UsageStat> {
         val usageStatsList = usageStatsDataSource.getUsageStats(startTime, endTime)
         return usageStatsList.map { usageStatModel ->
@@ -20,7 +20,7 @@ class DefaultUsageStatsRepository @Inject constructor(
     override fun getUsageTimeForPackage(
         startTime: Long,
         endTime: Long,
-        packageName: String,
+        packageName: String
     ): Long {
         val usageStatsList = getUsageStats(startTime, endTime)
         return usageStatsList.firstOrNull { it.packageName == packageName }?.totalTimeInForeground
@@ -30,7 +30,7 @@ class DefaultUsageStatsRepository @Inject constructor(
     override fun getUsageStatForPackages(
         startTime: Long,
         endTime: Long,
-        vararg packageNames: String,
+        vararg packageNames: String
     ): List<UsageStat> {
         val usageStatList = getUsageStats(startTime, endTime)
         return packageNames.map { packageName ->
@@ -38,7 +38,7 @@ class DefaultUsageStatsRepository @Inject constructor(
                 packageName,
                 usageStatList.find {
                     packageName == it.packageName
-                }?.totalTimeInForeground ?: 0,
+                }?.totalTimeInForeground ?: 0
             )
         }
     }
@@ -46,7 +46,7 @@ class DefaultUsageStatsRepository @Inject constructor(
     override fun getUsageStatForPackages(
         startTime: Long,
         endTime: Long,
-        packageNames: List<String>,
+        packageNames: List<String>
     ): List<UsageStat> {
         val usageStatList = getUsageStats(startTime, endTime)
         return packageNames.map { packageName ->
@@ -54,7 +54,7 @@ class DefaultUsageStatsRepository @Inject constructor(
                 packageName,
                 usageStatList.find {
                     packageName == it.packageName
-                }?.totalTimeInForeground ?: 0,
+                }?.totalTimeInForeground ?: 0
             )
         }
     }

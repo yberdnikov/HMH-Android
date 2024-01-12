2450fbc34d5025ddeaa6f221c3898dec1f7a754f
memoryBangwool <jihyuneee01@gmail.com>
2024-01-12 18:37:00 +0900

    [feat]: add MypageService

--- core/network/src/main/java/com/hmh/hamyeonham/core/network/mypage/MypageService.kt
+++ core/network/src/main/java/com/hmh/hamyeonham/core/network/mypage/MypageService.kt
@@ -1,4 +1,11 @@
 package com.hmh.hamyeonham.core.network.mypage
 
+import com.hmh.hamyeonham.core.network.model.BaseResponse
+import com.hmh.hamyeonham.core.network.mypage.model.UserInfoResponse
+import retrofit2.http.GET
+import retrofit2.http.Header
+
 interface MypageService {
+    @GET("api/v1/user")
+    suspend fun getUserInfo(@Header("Authorization") token: String): BaseResponse<UserInfoResponse>
 }

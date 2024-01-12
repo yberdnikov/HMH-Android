98980d529ed6eac3948f0e0599f069ced46b7ee7
memoryBangwool <jihyuneee01@gmail.com>
2024-01-12 13:41:27 +0900

    [feat]: add adapter call back function

--- feature/challenge/src/main/java/com/hmh/hamyeonham/challenge/appselection/AppselectionAdapter.kt
+++ feature/challenge/src/main/java/com/hmh/hamyeonham/challenge/appselection/AppselectionAdapter.kt
@@ -6,7 +6,7 @@ import androidx.recyclerview.widget.ListAdapter
 import com.hmh.hamyeonham.common.view.ItemDiffCallback
 import com.hmh.hamyeonham.feature.challenge.databinding.ItemAppBinding
 
-class AppselectionAdapter :
+class AppselectionAdapter(private val onAppCheckboxClicked: (Int) -> Unit) :
     ListAdapter<String, AppselectionViewHolder>(
         ItemDiffCallback(onItemsTheSame = { oldItem, newItem ->
             oldItem == newItem
@@ -18,7 +18,7 @@ class AppselectionAdapter :
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AppselectionViewHolder {
         val inflater = LayoutInflater.from(parent.context)
         val binding = ItemAppBinding.inflate(inflater, parent, false)
-        return AppselectionViewHolder(binding, parent.context)
+        return AppselectionViewHolder(binding, parent.context, onAppCheckboxClicked)
     }
 
     override fun onBindViewHolder(holder: AppselectionViewHolder, position: Int) {

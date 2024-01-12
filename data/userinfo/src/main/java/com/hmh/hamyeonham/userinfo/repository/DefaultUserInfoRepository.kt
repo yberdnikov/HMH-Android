7e1aaa0a2315ce303aa1894ccd5295f7684ac484
memoryBangwool <jihyuneee01@gmail.com>
2024-01-12 21:55:02 +0900

    [feat]: defaultUserInfoRepository 구조 변경

--- data/userinfo/src/main/java/com/hmh/hamyeonham/userinfo/repository/DefaultUserInfoRepository.kt
+++ data/userinfo/src/main/java/com/hmh/hamyeonham/userinfo/repository/DefaultUserInfoRepository.kt
@@ -2,14 +2,12 @@ package com.hmh.hamyeonham.userinfo.repository
 
 import com.hmh.hamyeonham.userinfo.datasource.UserInfoNetwork
 import com.hmh.hamyeonham.userinfo.model.UserInfo
-import com.hmh.hamyeonham.userinfo.model.UserInfoResponse
 import javax.inject.Inject
 
 class DefaultUserInfoRepository @Inject constructor(
     private val userInfoNetwork: UserInfoNetwork
 ) : UserInfoRepository {
-    override fun getUserInfo(): Result<UserInfo> {
-        val userInfoModel = userInfoNetwork.getUserInfoModel()
-        return UserInfo(userInfoModel.name, userInfoModel.point)
+    override suspend fun getUserInfo(): Result<UserInfo> {
+        return runCatching { userInfoNetwork.getUserInfoModel().toUserInfo() }
     }
 }

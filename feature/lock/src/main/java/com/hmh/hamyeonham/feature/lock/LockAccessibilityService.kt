f842931bd529ebd0662df57b6c3f4f3f122dcd60
KwakEuiJin <yuijin0909@gmail.com>
2024-01-12 13:40:24 +0900

    [feat]: Change Loop Change State

--- feature/lock/src/main/java/com/hmh/hamyeonham/feature/lock/LockAccessibilityService.kt
+++ feature/lock/src/main/java/com/hmh/hamyeonham/feature/lock/LockAccessibilityService.kt
@@ -6,43 +6,21 @@ import android.view.accessibility.AccessibilityEvent
 import com.hmh.hamyeonham.common.time.getCurrentDayStartEndEpochMillis
 import com.hmh.hamyeonham.usagestats.usecase.GetUsageGoalsUseCase
 import com.hmh.hamyeonham.usagestats.usecase.GetUsageStatFromPackageUseCase
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.isActive
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
-class OnBoardingAccessibilityService @Inject constructor(
+class LockAccessibilityService @Inject constructor(
     private val getUsageStatFromPackageUseCase: GetUsageStatFromPackageUseCase,
     private val getUsageGoalsUseCase: GetUsageGoalsUseCase,
 ) : AccessibilityService() {
 
-    private var usageCheckJob: Job? = null
-    private var currentPackageName: String? = null
-
-    override fun onServiceConnected() {
-        super.onServiceConnected()
-        startUsageCheck()
-    }
-
-    private fun startUsageCheck() {
-        usageCheckJob = GlobalScope.launch {
-            while (isActive) {
-                delay(10000)
-                checkUsage(currentPackageName.toString())
-            }
-        }
-    }
-
     override fun onAccessibilityEvent(event: AccessibilityEvent) {
         if (event.eventType == AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED) {
-            currentPackageName = event.packageName.toString()
-            checkUsage(currentPackageName.toString())
+            checkUsage(event)
         }
     }
 
-    private fun checkUsage(packageName: String) {
+    private fun checkUsage(event: AccessibilityEvent) {
+        val packageName = event.packageName?.toString() ?: return
         val (startTime, endTime) = getCurrentDayStartEndEpochMillis()
         val usageStats = getUsageStatFromPackageUseCase(
             startTime = startTime,
@@ -51,7 +29,6 @@ class OnBoardingAccessibilityService @Inject constructor(
         )
         getUsageGoalsUseCase().firstOrNull { it.packageName == packageName }?.let { myGoal ->
             if (usageStats > myGoal.goalTime) {
-                currentPackageName = null
                 Intent(this, LockActivity::class.java).apply {
                     addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
                     addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
@@ -62,9 +39,4 @@ class OnBoardingAccessibilityService @Inject constructor(
     }
 
     override fun onInterrupt() {}
-
-    override fun onDestroy() {
-        usageCheckJob?.cancel()
-        super.onDestroy()
-    }
 }

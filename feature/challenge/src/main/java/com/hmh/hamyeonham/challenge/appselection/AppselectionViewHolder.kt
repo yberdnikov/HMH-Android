4815e905a1205d06d7741ba62ada7e2c1a1801fb
memoryBangwool <jihyuneee01@gmail.com>
2024-01-12 12:45:14 +0900

    [feat]: add checkbox custom color

--- feature/challenge/src/main/java/com/hmh/hamyeonham/challenge/appselection/AppselectionViewHolder.kt
+++ feature/challenge/src/main/java/com/hmh/hamyeonham/challenge/appselection/AppselectionViewHolder.kt
@@ -1,6 +1,8 @@
 package com.hmh.hamyeonham.challenge.appselection
 
 import android.content.Context
+import android.content.res.ColorStateList
+import androidx.core.content.ContextCompat.getColor
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.hmh.hamyeonham.common.context.getAppIconFromPackageName
 import com.hmh.hamyeonham.common.context.getAppNameFromPackageName
@@ -11,5 +13,38 @@ class AppselectionViewHolder(private val binding: ItemAppBinding, private val co
     fun onBind(packageName: String) {
         binding.tvAppname.text = context.getAppNameFromPackageName(packageName)
         binding.ivAppicon.setImageDrawable(context.getAppIconFromPackageName(packageName))
+        initCheckBoxColor()
+        setCheckBoxButtonListener()
+    }
+    private fun initCheckBoxColor() {
+        val colorStateList = ColorStateList.valueOf(
+            getColor(
+                context,
+                com.hmh.hamyeonham.core.designsystem.R.color.white_text
+            )
+        )
+        binding.cbApp.buttonTintList = colorStateList
+    }
+
+    private fun setCheckBoxButtonListener() {
+        binding.cbApp.setOnClickListener {
+            if (binding.cbApp.isChecked) {
+                val colorStateList = ColorStateList.valueOf(
+                    getColor(
+                        context,
+                        com.hmh.hamyeonham.core.designsystem.R.color.blue_purple_text
+                    )
+                )
+                binding.cbApp.buttonTintList = colorStateList
+            } else {
+                val colorStateList = ColorStateList.valueOf(
+                    getColor(
+                        context,
+                        com.hmh.hamyeonham.core.designsystem.R.color.white_text
+                    )
+                )
+                binding.cbApp.buttonTintList = colorStateList
+            }
+        }
     }
 }

713b4f3f8657b143e90f3736d43c2cc861bb86a2
KwakEuiJin <yuijin0909@gmail.com>
2024-01-13 23:42:18 +0900

    [feat]: Add AppAddActivity

--- feature/challenge/src/main/java/com/hmh/hamyeonham/challenge/appadd/AppAddActivity.kt
+++ feature/challenge/src/main/java/com/hmh/hamyeonham/challenge/appadd/AppAddActivity.kt
@@ -8,12 +8,15 @@ import androidx.lifecycle.flowWithLifecycle
 import androidx.lifecycle.lifecycleScope
 import com.hmh.hamyeonham.common.view.viewBinding
 import com.hmh.hamyeonham.feature.challenge.databinding.ActivityAppAddBinding
+import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 
+@AndroidEntryPoint
 class AppAddActivity : AppCompatActivity() {
     companion object {
         const val SELECTED_APPS = "selected_apps"
+        const val GOAL_TIME = "goal_time"
     }
 
     private val binding by viewBinding(ActivityAppAddBinding::inflate)
@@ -49,11 +52,9 @@ class AppAddActivity : AppCompatActivity() {
 
             1 -> {
                 val intent = Intent(this@AppAddActivity, AppAddActivity::class.java).apply {
-                    putExtra(
-                        SELECTED_APPS,
-                        viewModel.state.value.selectedApp.toTypedArray()
-                    )
-                    putExtra("goal_time", viewModel.state.value.goalTime)
+                    val selectedApps = viewModel.state.value.selectedApp
+                    putExtra(SELECTED_APPS, selectedApps.toTypedArray())
+                    putExtra(GOAL_TIME, viewModel.state.value.goalTime)
                 }
                 setResult(RESULT_OK, intent)
                 finish()
--- feature/challenge/src/main/java/com/hmh/hamyeonham/challenge/appadd/appselection/AppSelectionFragment.kt
+++ feature/challenge/src/main/java/com/hmh/hamyeonham/challenge/appadd/appselection/AppSelectionFragment.kt
@@ -1,19 +1,22 @@
 package com.hmh.hamyeonham.challenge.appadd.appselection
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.hmh.hamyeonham.challenge.appadd.AppAddViewModel
 import com.hmh.hamyeonham.common.view.viewBinding
 import com.hmh.hamyeonham.feature.challenge.databinding.FrargmentAppSelectionBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class AppSelectionFragment : Fragment() {
     private val binding by viewBinding(FrargmentAppSelectionBinding::bind)
+    private val viewModel by viewModels<AppSelectionViewModel>()
     private val activityViewModel by activityViewModels<AppAddViewModel>()
 
     override fun onCreateView(
@@ -40,9 +43,13 @@ class AppSelectionFragment : Fragment() {
                 onAppCheckboxUnClicked = ::onAppCheckboxUnClicked
             )
             layoutManager = LinearLayoutManager(requireContext())
-            val usageStaticsAdapter = binding.rvAppSelection.adapter as? AppSelectionAdapter
-            usageStaticsAdapter?.submitList(getInstalledApps())
         }
+        setViewPager()
+    }
+
+    private fun setViewPager() {
+        val appSelectionAdapter = binding.rvAppSelection.adapter as? AppSelectionAdapter
+        appSelectionAdapter?.submitList(viewModel.getInstalledApps())
     }
 
     private fun onAppCheckboxClicked(packageName: String) {
@@ -54,17 +61,6 @@ class AppSelectionFragment : Fragment() {
     private fun onAppCheckboxUnClicked(packageName: String) {
         activityViewModel.updateState {
             copy(selectedApp = selectedApp - packageName)
-        }
-    }
-
-
-    private fun getInstalledApps(): List<String> {
-        val intent = Intent(Intent.ACTION_MAIN).apply {
-            addCategory(Intent.CATEGORY_LAUNCHER)
-        }
-        val resolveInfoList = requireContext().packageManager.queryIntentActivities(intent, 0)
-        return resolveInfoList.map {
-            it.activityInfo.packageName
         }
     }
 }

dd1e66a2d5fbfba9aa27c8ee147bd3f5440de7c7
memoryBangwool <jihyuneee01@gmail.com>
2024-01-14 14:53:02 +0900

    [refact]: defaultUsageGoalsRepository구조 변경

--- data/usagestats/src/main/java/com/hmh/hamyeonham/usagestats/repository/DefaultUsageGoalsRepository.kt
+++ data/usagestats/src/main/java/com/hmh/hamyeonham/usagestats/repository/DefaultUsageGoalsRepository.kt
@@ -6,10 +6,10 @@ import com.hmh.hamyeonham.usagestats.model.UsageGoal
 import javax.inject.Inject
 
 class DefaultUsageGoalsRepository @Inject constructor(
-    private val usageGoalService: UsageGoalService
+    private val usageGoalService: UsageGoalService,
 ) : UsageGoalsRepository {
-    override suspend fun getUsageGoals(): List<UsageGoal> {
-        return usageGoalService.getUsageGoal().data.toUsageGoalList()
+    override suspend fun getUsageGoals(): Result<List<UsageGoal>> {
+        return runCatching { usageGoalService.getUsageGoal().data.toUsageGoalList() }
     }
 
     override suspend fun getUsageGoalTime(packageName: String): Long {

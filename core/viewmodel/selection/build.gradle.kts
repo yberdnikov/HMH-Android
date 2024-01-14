84f193273755a6928a4288622315c7015cc9be49
KwakEuiJin <yuijin0909@gmail.com>
2024-01-15 02:11:49 +0900

    [feat]: Add Selection ViewModel Module

---
+++ core/viewmodel/selection/build.gradle.kts
@@ -0,0 +1,12 @@
+@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
+plugins {
+    hmh("feature")
+}
+
+android {
+    namespace = "com.hmh.hamyeonham.core.viewmodel.selection"
+}
+
+dependencies {
+    implementation(projects.domain.challenge)
+}

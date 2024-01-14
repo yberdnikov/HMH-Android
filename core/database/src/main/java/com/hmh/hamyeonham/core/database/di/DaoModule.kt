3f787103c1e8f4f3583da141744aedff2287b4ce
KwakEuiJin <yuijin0909@gmail.com>
2024-01-14 20:48:28 +0900

    [feat]: Add DaoModule

---
+++ core/database/src/main/java/com/hmh/hamyeonham/core/database/di/DaoModule.kt
@@ -0,0 +1,18 @@
+package com.hmh.hamyeonham.core.database.di
+
+import com.hmh.hamyeonham.core.database.HMHRoomDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DaoModule {
+    @Provides
+    @Singleton
+    fun providesUsageGoalsDao(
+        database: HMHRoomDatabase,
+    ) = database.usageGoalsDao()
+}

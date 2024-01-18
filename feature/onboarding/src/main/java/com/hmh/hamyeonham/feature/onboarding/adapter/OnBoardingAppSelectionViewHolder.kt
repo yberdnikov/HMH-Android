41cb17d8f81701bcce587124068984e235872f21
kangyuri1114 <yuri3334@gmail.com>
2024-01-18 20:07:43 +0900

    [fix]: 아이템 선택 영역 확장

--- feature/onboarding/src/main/java/com/hmh/hamyeonham/feature/onboarding/adapter/OnBoardingAppSelectionViewHolder.kt
+++ feature/onboarding/src/main/java/com/hmh/hamyeonham/feature/onboarding/adapter/OnBoardingAppSelectionViewHolder.kt
@@ -9,24 +9,27 @@ import com.hmh.hamyeonham.feature.onboarding.databinding.ItemAddAppBinding
 class OnBoardingAppSelectionViewHolder(
     private val binding: ItemAddAppBinding,
     private val onAppCheckboxClicked: (String) -> Unit,
-    private val onAppCheckboxUnClicked: (String) -> Unit
+    private val onAppCheckboxUnClicked: (String) -> Unit,
 ) : ViewHolder(binding.root) {
     fun onBind(packageName: String) {
         binding.run {
             val context = binding.root.context
             tvAppname.text = context.getAppNameFromPackageName(packageName)
             ivAppicon.setImageDrawable(context.getAppIconFromPackageName(packageName))
+            cbApp.isClickable = false
         }
         setCheckBoxButtonListener(packageName)
     }
 
     private fun setCheckBoxButtonListener(packageName: String) {
-        binding.cbApp.setOnClickListener {
+        binding.root.setOnClickListener {
             if (adapterPosition != RecyclerView.NO_POSITION) {
                 if (binding.cbApp.isChecked) {
-                    onAppCheckboxClicked(packageName)
-                } else {
+                    binding.cbApp.isChecked = false
                     onAppCheckboxUnClicked(packageName)
+                } else {
+                    binding.cbApp.isChecked = true
+                    onAppCheckboxClicked(packageName)
                 }
             }
         }

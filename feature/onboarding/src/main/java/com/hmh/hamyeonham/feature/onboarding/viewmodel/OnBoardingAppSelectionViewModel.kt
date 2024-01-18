85ae0396d7f7ba5954c4b7f2495b81b38b833f62
kangyuri1114 <yuri3334@gmail.com>
2024-01-19 07:48:44 +0900

    [feat]: 앱 선택 화면 가로 대응

--- feature/onboarding/src/main/java/com/hmh/hamyeonham/feature/onboarding/viewmodel/OnBoardingAppSelectionViewModel.kt
+++ feature/onboarding/src/main/java/com/hmh/hamyeonham/feature/onboarding/viewmodel/OnBoardingAppSelectionViewModel.kt
@@ -1,15 +1,33 @@
 package com.hmh.hamyeonham.feature.onboarding.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.hmh.hamyeonham.challenge.repository.DeviceRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
+
+data class AppSelectionState(
+    val selectedApp: List<String> = emptyList(),
+)
 
 @HiltViewModel
 class OnBoardingAppSelectionViewModel @Inject constructor(
-    private val deviceRepository: DeviceRepository
+    private val deviceRepository: DeviceRepository,
 ) : ViewModel() {
+    private val _state = MutableStateFlow(AppSelectionState())
+    val state = _state.asStateFlow()
     fun getInstalledApps(): List<String> {
         return deviceRepository.getInstalledApps()
+    }
+
+    fun updateState(transform: suspend AppSelectionState.() -> AppSelectionState) {
+        viewModelScope.launch {
+            val currentState = state.value
+            val newState = currentState.transform()
+            _state.value = newState
+        }
     }
 }

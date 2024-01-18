dbf49a2034c9293ebb5ee0606f90fa96b2621512
memoryBangwool <jihyuneee01@gmail.com>
2024-01-19 04:58:10 +0900

    [feat]: 검색 기능 구현

--- feature/challenge/src/main/java/com/hmh/hamyeonham/challenge/appadd/appselection/AppSelectionFragment.kt
+++ feature/challenge/src/main/java/com/hmh/hamyeonham/challenge/appadd/appselection/AppSelectionFragment.kt
@@ -1,6 +1,9 @@
 package com.hmh.hamyeonham.challenge.appadd.appselection
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,6 +14,7 @@ import androidx.lifecycle.flowWithLifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.hmh.hamyeonham.challenge.appadd.AppAddViewModel
+import com.hmh.hamyeonham.common.context.getAppNameFromPackageName
 import com.hmh.hamyeonham.common.view.viewBinding
 import com.hmh.hamyeonham.feature.challenge.databinding.FrargmentAppSelectionBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -26,7 +30,7 @@ class AppSelectionFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
-        savedInstanceState: Bundle?
+        savedInstanceState: Bundle?,
     ): View {
         return FrargmentAppSelectionBinding.inflate(inflater, container, false).root
     }
@@ -34,6 +38,7 @@ class AppSelectionFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initViews()
+        initSearchBar()
         collectState()
         viewModel.getInstalledApps()
     }
@@ -42,10 +47,52 @@ class AppSelectionFragment : Fragment() {
         initAppSelectionRecyclerAdapter()
     }
 
+    private fun initSearchBar() {
+        binding.etSearchbar.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                setRecyclerViewWithFilter(s.toString())
+            }
+
+            override fun afterTextChanged(s: Editable?) {}
+        })
+    }
+
+    private fun setRecyclerViewWithFilter(filter: String) {
+        val appSelectionAdapter = binding.rvAppSelection.adapter as? AppSelectionAdapter
+        val selectedApp = viewModel.state.value.selectedApp
+        val newAppList = selectedApp.filter {
+            (context?.getAppNameFromPackageName(it) ?: "").contains(filter)
+        }
+        appSelectionAdapter?.submitList(
+            newAppList.map {
+                Pair(
+                    it,
+                    checkIfAppIsSelected(it),
+                )
+            },
+        )
+    }
+
+    private fun checkIfAppIsSelected(app: String): Boolean {
+        val selcetedApps = activityViewModel.state.value.selectedApp
+        val isChecked = selcetedApps.find { it == app } != null
+        if (isChecked) Log.d("checked app", app)
+        return isChecked
+    }
+
     private fun collectState() {
         viewModel.state.flowWithLifecycle(lifecycle).onEach { state ->
             val appSelectionAdapter = binding.rvAppSelection.adapter as? AppSelectionAdapter
-            appSelectionAdapter?.submitList(state.selectedApp)
+            appSelectionAdapter?.submitList(
+                state.selectedApp.map {
+                    Pair(
+                        it,
+                        checkIfAppIsSelected(it),
+                    )
+                },
+            )
         }.launchIn(lifecycleScope)
     }
 
@@ -63,11 +110,16 @@ class AppSelectionFragment : Fragment() {
         activityViewModel.updateState {
             copy(selectedApp = selectedApp + packageName)
         }
+        for (i in activityViewModel.state.value.selectedApp)
+            Log.d("selected apps", i)
     }
 
     private fun onAppCheckboxUnClicked(packageName: String) {
         activityViewModel.updateState {
             copy(selectedApp = selectedApp - packageName)
         }
+        Log.d("delete viewmodel", "")
+        for (i in activityViewModel.state.value.selectedApp)
+            Log.d("selected apps", i)
     }
 }

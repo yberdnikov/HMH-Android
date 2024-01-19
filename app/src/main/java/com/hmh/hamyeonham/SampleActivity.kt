b273276492cf2b38450853e3390aa17d9d48df60
kangyuri1114 <yuri3334@gmail.com>
2024-01-19 09:28:54 +0900

    [feat]: 온보딩 세로 모드 설정

--- app/src/main/java/com/hmh/hamyeonham/SampleActivity.kt
+++ app/src/main/java/com/hmh/hamyeonham/SampleActivity.kt
@@ -9,8 +9,7 @@ import androidx.core.splashscreen.SplashScreen
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.hmh.hamyeonham.common.view.viewBinding
 import com.hmh.hamyeonham.databinding.ActivitySampleBinding
-import com.hmh.hamyeonham.feature.login.LoginActivity
-import com.hmh.hamyeonham.feature.onboarding.OnBoardingActivity
+import com.hmh.hamyeonham.feature.main.MainActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -22,7 +21,7 @@ class SampleActivity : AppCompatActivity() {
         val splashScreen = installSplashScreen()
         initSplashAnimation(splashScreen)
         setContentView(binding.root)
-        startActivity(Intent(this, OnBoardingActivity::class.java))
+        startActivity(Intent(this, MainActivity::class.java))
         finish()
     }
 

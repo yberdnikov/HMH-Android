4a830834d631b2b7237eb0c67d21e1e1bc2b9a88
jihyun0v0 <jihyuneee01@gmail.com>
2024-03-12 12:53:50 +0900

    [feat]: 뷰홀더 로티, 설명 로직 추가, 동영상 로직 삭제

--- feature/main/src/main/java/com/hmh/hamyeonham/feature/main/home/UsageStaticsTotalViewHolder.kt
+++ feature/main/src/main/java/com/hmh/hamyeonham/feature/main/home/UsageStaticsTotalViewHolder.kt
@@ -1,8 +1,6 @@
 package com.hmh.hamyeonham.feature.main.home
 
 import android.content.Context
-import android.net.Uri
-import android.view.View
 import androidx.core.content.ContextCompat.getString
 import androidx.recyclerview.widget.RecyclerView
 import com.hmh.hamyeonham.common.context.colorSecondStrAndBindText
@@ -10,8 +8,6 @@ import com.hmh.hamyeonham.common.time.convertTimeToString
 import com.hmh.hamyeonham.common.view.initAndStartProgressBarAnimation
 import com.hmh.hamyeonham.feature.main.R
 import com.hmh.hamyeonham.feature.main.databinding.ItemUsagestaticTotalBinding
-import com.hmh.hamyeonham.usagestats.model.BlackHole
-import com.hmh.hamyeonham.usagestats.model.UsageStatusAndGoal
 
 class UsageStaticsTotalViewHolder(
     private val binding: ItemUsagestaticTotalBinding,
@@ -36,60 +32,37 @@ class UsageStaticsTotalViewHolder(
             binding.pbTotalUsage,
             usageStatusAndGoal.usedPercentage,
         )
-        bindBlackhole(usageStaticsModel, usageStaticsModel.challengeSuccess)
-        setBlackholeLoop()
+        bindBlackhole(usageStaticsModel)
     }
 
-    fun bindBlackhole(usageStaticsModel: UsageStaticsModel, challengeSucess: Boolean) {
-        if (challengeSucess) {
-            bindBlackHoleVideo(usageStaticsModel.usageStatusAndGoal.getBlackHoleUri())
-            bindBlackholeDescriptionWithModel(
-                usageStaticsModel.usageStatusAndGoal,
-                usageStaticsModel.userName,
-            )
-        } else {
-            binding.vvBlackhole.visibility = View.GONE
-            bindBlackholeDescriptionWithString(
-                getString(context, R.string.blackhole_fail),
-            )
+    fun bindBlackhole(usageStaticsModel: UsageStaticsModel) {
+        bindBlackHoleAnimation(usageStaticsModel)
+        bindBlackHoleDiscription(usageStaticsModel)
+    }
+
+    private fun bindBlackHoleAnimation(usageStaticsModel: UsageStaticsModel) {
+        val lottieFile = when (usageStaticsModel.usageStatusAndGoal.blackHoleLevel) {
+            0 -> R.raw.lottie_blackhole0
+            1 -> R.raw.lottie_blackhole1
+            2 -> R.raw.lottie_blackhole2
+            3 -> R.raw.lottie_blackhole3
+            4 -> R.raw.lottie_blackhole4
+            5 -> R.raw.lottie_blackhole5
+            else -> 0
         }
+        binding.lavBlackhole.setAnimation(lottieFile)
     }
 
-    private fun setBlackholeLoop() {
-        binding.vvBlackhole.setOnCompletionListener {
-            binding.vvBlackhole.start()
+    private fun bindBlackHoleDiscription(usageStaticsModel: UsageStaticsModel) {
+        val blackHoleDiscription: Int = when (usageStaticsModel.usageStatusAndGoal.blackHoleLevel) {
+            0 -> R.string.blackhole0
+            1 -> R.string.blackhole1
+            2 -> R.string.blackhole2
+            3 -> R.string.blackhole3
+            4 -> R.string.blackhole4
+            5 -> R.string.blackhole5
+            else -> 0
         }
-    }
-
-    private fun bindBlackHoleVideo(uri: String) {
-        val uri = Uri.parse(uri)
-        binding.vvBlackhole.run {
-            setVideoURI(uri)
-            requestFocus()
-            setOnPreparedListener {
-                it.start()
-            }
-        }
-    }
-
-    private fun bindBlackholeDescriptionWithModel(
-        userStatusAndGoal: UsageStatusAndGoal,
-        userName: String,
-    ) {
-        binding.tvBlackholeDescription.text = when (userStatusAndGoal.blackHole) {
-            BlackHole.FIRST -> userName
-            else -> ""
-        } + userStatusAndGoal.getBlackHoleDescription()
-        binding.tvBlackholeDescription.bringToFront()
-    }
-
-    private fun bindBlackholeDescriptionWithString(
-        str: String,
-    ) {
-        binding.tvBlackholeDescription.text = str
-    }
-
-    fun pause() {
-        binding.vvBlackhole.pause()
+        binding.tvBlackholeDescription.text = getString(context, blackHoleDiscription)
     }
 }

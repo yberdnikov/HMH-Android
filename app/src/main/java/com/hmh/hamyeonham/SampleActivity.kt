0d8e6328d4cc0b5cc77ffbb7558eb6db0db67ef9
kangyuri1114 <yuri3334@gmail.com>
2024-03-17 23:09:22 +0900

    [feat]: 챌린지 앱 편집 UI 수정

--- app/src/main/java/com/hmh/hamyeonham/SampleActivity.kt
+++ app/src/main/java/com/hmh/hamyeonham/SampleActivity.kt
@@ -7,6 +7,7 @@ import android.view.animation.AnimationUtils
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.hmh.hamyeonham.challenge.appadd.AppAddActivity
 import com.hmh.hamyeonham.common.view.viewBinding
 import com.hmh.hamyeonham.databinding.ActivitySampleBinding
 import com.hmh.hamyeonham.feature.login.LoginActivity
@@ -23,7 +24,7 @@ class SampleActivity : AppCompatActivity() {
         val splashScreen = installSplashScreen()
         initSplashAnimation(splashScreen)
         setContentView(binding.root)
-        startActivity(Intent(this, LoginActivity::class.java))
+        startActivity(Intent(this, AppAddActivity::class.java))
         finish()
     }
 

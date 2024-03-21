6364f238ffc0a2940487e0e075545bb96cd3b935
kangyuri1114 <yuri3334@gmail.com>
2024-03-21 16:03:32 +0900

    [fix]: 시작 액티비티 기존 화면으로 수정

--- app/src/main/java/com/hmh/hamyeonham/SampleActivity.kt
+++ app/src/main/java/com/hmh/hamyeonham/SampleActivity.kt
@@ -7,12 +7,9 @@ import android.view.animation.AnimationUtils
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import com.hmh.hamyeonham.challenge.appadd.AppAddActivity
 import com.hmh.hamyeonham.common.view.viewBinding
 import com.hmh.hamyeonham.databinding.ActivitySampleBinding
 import com.hmh.hamyeonham.feature.login.LoginActivity
-import com.hmh.hamyeonham.feature.main.MainActivity
-import com.hmh.hamyeonham.feature.onboarding.OnBoardingActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -24,7 +21,7 @@ class SampleActivity : AppCompatActivity() {
         val splashScreen = installSplashScreen()
         initSplashAnimation(splashScreen)
         setContentView(binding.root)
-        startActivity(Intent(this, AppAddActivity::class.java))
+        startActivity(Intent(this, LoginActivity::class.java))
         finish()
     }
 

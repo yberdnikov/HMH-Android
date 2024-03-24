8cdb5e9b045a33144df200601b02da1c01d753fe
kangyuri1114 <yuri3334@gmail.com>
2024-03-24 16:53:01 +0900

    [delete]: 필요없는 종속성 삭제

--- feature/onboarding/build.gradle.kts
+++ feature/onboarding/build.gradle.kts
@@ -1,7 +1,6 @@
 @Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
 plugins {
     hmh("feature")
-    alias(libs.plugins.kotlin.android)
 }
 
 android {
@@ -18,7 +17,4 @@ dependencies {
 
     implementation(projects.domain.login)
     implementation(projects.domain.challenge)
-    implementation(libs.appcompat)
-    implementation(libs.material)
-    implementation(libs.constraintlayout)
 }

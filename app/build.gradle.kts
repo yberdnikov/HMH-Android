4815d730959a3a0b78fe6eee7193bf9ac38df4a3
KwakEuiJin <yuijin0909@gmail.com>
2024-05-13 09:01:33 +0900

    [fix]: app point 종속성 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -71,6 +71,7 @@ dependencies {
     implementation(projects.domain.login)
     implementation(projects.domain.challenge)
     implementation(projects.domain.onboarding)
+    implementation(projects.domain.point)
 
     // Data
     implementation(projects.data.usagestats)
@@ -79,6 +80,7 @@ dependencies {
     implementation(projects.data.challenge)
     implementation(projects.data.device)
     implementation(projects.data.onboarding)
+    implementation(projects.data.point)
 
     // Core
     implementation(projects.core.common)

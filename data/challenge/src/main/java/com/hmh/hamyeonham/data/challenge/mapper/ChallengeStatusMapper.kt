cbf05556cac5a973669337b505bd4a13bd35bb2d
jihyun0v0 <jihyuneee01@gmail.com>
2024-05-17 15:26:41 +0900

    [fix]: api 수정에 따른 변경 원복

--- data/challenge/src/main/java/com/hmh/hamyeonham/data/challenge/mapper/ChallengeStatusMapper.kt
+++ data/challenge/src/main/java/com/hmh/hamyeonham/data/challenge/mapper/ChallengeStatusMapper.kt
@@ -4,7 +4,7 @@ import com.hmh.hamyeonham.challenge.model.ChallengeStatus
 
 internal fun List<String>.toStatusList(todayIndex: Int, period: Int): List<ChallengeStatus.Status> {
     // 원본 리스트를 ChallengeStatus.Status 리스트로 변환
-    val statusList = this.map {
+    return this.map {
         when (it) {
             ChallengeStatus.Status.NONE.value -> ChallengeStatus.Status.NONE
             ChallengeStatus.Status.UNEARNED.value -> ChallengeStatus.Status.UNEARNED
@@ -12,17 +12,9 @@ internal fun List<String>.toStatusList(todayIndex: Int, period: Int): List<Chall
             ChallengeStatus.Status.FAILURE.value -> ChallengeStatus.Status.FAILURE
             else -> ChallengeStatus.Status.NONE
         }
-    }.toMutableList()
-
-    // todayIndex가 유효한 경우
-    if (todayIndex > -1) {
-        // 필요한 만큼 NONE 상태를 추가
-        val additionalItemsCount = period - todayIndex
-        val additionalItems = List(additionalItemsCount) { ChallengeStatus.Status.NONE }
-        // 추가 항목을 원본 리스트에 덧붙임
-        statusList.addAll(additionalItems)
-        //today 인덱스에 TODAY 상태를 설정
-        statusList[todayIndex] = ChallengeStatus.Status.TODAY
+    }.toMutableList().also {
+        // todayIndex가 유효한 경우 today 인덱스에 TODAY 상태를 설정
+        if (todayIndex > -1)
+            it[todayIndex] = ChallengeStatus.Status.TODAY
     }
-    return statusList
 }

43d5f702399be0c3577132bdd5a2776cff24fe4e
KwakEuiJin <yuijin0909@gmail.com>
2024-05-29 01:43:59 +0900

    [feat]: 자동 잠금 화면 계산 로직 추가

--- core/service/src/main/java/com/hmh/hamyeonham/core/service/LockAccessibilityService.kt
+++ core/service/src/main/java/com/hmh/hamyeonham/core/service/LockAccessibilityService.kt
@@ -8,12 +8,13 @@ import androidx.lifecycle.ProcessLifecycleOwner
 import androidx.lifecycle.lifecycleScope
 import com.hmh.hamyeonham.common.navigation.NavigationProvider
 import com.hmh.hamyeonham.common.time.getCurrentDayStartEndEpochMillis
+import com.hmh.hamyeonham.core.domain.usagegoal.model.UsageGoal
 import com.hmh.hamyeonham.lock.GetIsUnLockUseCase
 import com.hmh.hamyeonham.usagestats.usecase.GetUsageGoalsUseCase
 import com.hmh.hamyeonham.usagestats.usecase.GetUsageStatFromPackageUseCase
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.Job
-import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -23,8 +24,10 @@ class LockAccessibilityService : AccessibilityService() {
 
     @Inject
     lateinit var getUsageStatFromPackageUseCase: GetUsageStatFromPackageUseCase
+
     @Inject
     lateinit var getUsageGoalsUseCase: GetUsageGoalsUseCase
+
     @Inject
     lateinit var getUsageIsLockUseCase: GetIsUnLockUseCase
 
@@ -32,18 +35,19 @@ class LockAccessibilityService : AccessibilityService() {
     lateinit var navigationProvider: NavigationProvider
 
     private var checkUsageJob: Job? = null
+    private var timerJob: Job? = null
 
     override fun onAccessibilityEvent(event: AccessibilityEvent) {
         if (getUsageIsLockUseCase()) return
         when (event.eventType) {
-            AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED -> {
-                checkUsageJob?.cancel()
-                checkUsageJob = null
-                checkUsageJob = monitorAndLockIfExceedsUsageGoal(event)
-            }
-
+            AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED -> resetCheckUsageJob(event)
             else -> Unit
         }
+    }
+
+    private fun resetCheckUsageJob(event: AccessibilityEvent) {
+        releaseCheckUsageJob()
+        checkUsageJob = monitorAndLockIfExceedsUsageGoal(event)
     }
 
     private fun monitorAndLockIfExceedsUsageGoal(event: AccessibilityEvent): Job {
@@ -60,16 +64,51 @@ class LockAccessibilityService : AccessibilityService() {
             val myGoal = usageGoals.find { it.packageName == packageName } ?: return@launch
             Log.d("LockAccessibilityService", "checkUsage: $usageStats")
             Log.d("LockAccessibilityService", "checkUsage: ${myGoal.goalTime}")
-            if (usageStats > myGoal.goalTime) {
-                val intent = navigationProvider.toLock(packageName).apply {
-                    addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-                }
-                startActivity(intent)
+            checkLockApp(usageStats, myGoal, packageName)
+        }
+    }
+
+    private fun checkLockApp(
+        usageStats: Long,
+        myGoal: UsageGoal,
+        packageName: String
+    ) {
+        if (usageStats > myGoal.goalTime) {
+            moveToLock(packageName)
+        } else {
+            releaseTimerJob()
+            timerJob = ProcessLifecycleOwner.get().lifecycleScope.launch {
+                val remainingTime = myGoal.goalTime - usageStats
+                delay(remainingTime)
+                moveToLock(packageName)
             }
         }
     }
 
+    private fun releaseCheckUsageJob() {
+        checkUsageJob?.cancel()
+        checkUsageJob = null
+    }
+
+    private fun releaseTimerJob() {
+        timerJob?.cancel()
+        timerJob = null
+    }
+
+    private fun moveToLock(packageName: String) {
+        val intent = navigationProvider.toLock(packageName).apply {
+            addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        }
+        startActivity(intent)
+    }
+
     override fun onInterrupt() {}
+
+    override fun onDestroy() {
+        super.onDestroy()
+        releaseCheckUsageJob()
+        releaseTimerJob()
+    }
 }
 
 val lockAccessibilityServiceClassName: String =

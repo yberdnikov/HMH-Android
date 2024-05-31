a4e39a15520ea0fea49b20d51cbb281d19875c58
kangyuri1114 <yuri3334@gmail.com>
2024-05-31 15:50:01 +0900

    [fix]: handler 구현 -> coroutine 수정

--- app/src/main/java/com/hmh/hamyeonham/SampleActivity.kt
+++ app/src/main/java/com/hmh/hamyeonham/SampleActivity.kt
@@ -2,18 +2,21 @@ package com.hmh.hamyeonham
 
 import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.hmh.hamyeonham.common.view.viewBinding
 import com.hmh.hamyeonham.databinding.ActivitySampleBinding
 import com.hmh.hamyeonham.feature.login.LoginActivity
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class SampleActivity : AppCompatActivity() {
     private val binding by viewBinding(ActivitySampleBinding::inflate)
+    private val coroutineScope = CoroutineScope(Dispatchers.Main)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,14 +28,11 @@ class SampleActivity : AppCompatActivity() {
 
     private fun initLottieSplash() {
         binding.splashLottieAppLogo.playAnimation()
-        val handler = Handler(
-            Looper.getMainLooper()
-        )
-        handler.postDelayed(
-            {
-                navigateToLogin()
-            }, 4000
-        )
+
+        coroutineScope.launch{
+            delay(4000)
+            navigateToLogin ()
+        }
     }
 
     private fun navigateToLogin() {
